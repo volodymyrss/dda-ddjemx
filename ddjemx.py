4e183bae2ad91ed8b622031d494c427251d9b8e1
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2019-01-09 16:00:14 +0100

    jrmf uses rbp

--- ddjemx.py
+++ ddjemx.py
@@ -348,7 +348,18 @@ class JRMF(ddosa.DataAnalysis):
     def main(self):
         code='STD_%.3i'%(2**(-self.input_jbins.nchanpow))
         fn='jemx_rmf_%s.fits'%code
-        os.system('j_rebin_rmf binlist=%s outfile=%s'%(code,fn))
+
+        env = deepcopy(environ)
+        env['REP_BASE_PROD'] = env['CURRENT_IC']
+
+        subprocess.check_call(
+                        [
+                            'j_rebin_rmf',
+                            'binlist=%s'%code,
+                            'outfile=%s'%fn
+                        ],
+                        env=env,
+                    )
         self.rmf=da.DataFile(fn)
 
 class ProcessJSpectra(ddosa.DataAnalysis):
